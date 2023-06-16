2592adfcc22653a68a6a78c3dfd23283813fb009
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-06-15 21:09:17 -0300

    utilizando Transactional

--- src/main/java/org/acme/controller/CustomerController.java
+++ src/main/java/org/acme/controller/CustomerController.java
@@ -7,6 +7,7 @@ import org.acme.model.Customer;
 import org.acme.service.CustomerService;
 
 import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
@@ -32,6 +33,7 @@ public class CustomerController {
 	}
 
 	@POST
+	@Transactional
 	public void addCustomer(Customer customer) {
 		this.customerService.addCustomer(customer);
 	}
