9ce5ff6e80a23efa1d83e528a8f1003a77391c20
Pedro Rhamon <pedrorhamon16@gmail.com>
2023-06-15 21:06:24 -0300

    criando endpoints

--- src/main/java/org/acme/controller/CustomerController.java
+++ src/main/java/org/acme/controller/CustomerController.java
@@ -1,8 +1,33 @@
 package org.acme.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.acme.model.Customer;
+import org.acme.service.CustomerService;
+
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+
 /**
  * @author pedroRhamon
  */
+@Path(value = "/api/customer")
 public class CustomerController {
+
+	@Inject
+	CustomerService customerService;
+
+	@GET
+	public List<Customer> retrieveCustomers() {
+		List<Customer> customers = new ArrayList<>();
+		try {
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return customers;
+	}
 
 }
--- src/main/java/org/acme/service/CustomerService.java
+++ src/main/java/org/acme/service/CustomerService.java
@@ -1,8 +1,30 @@
 package org.acme.service;
 
+import java.util.List;
+
+import org.acme.model.Customer;
+import org.acme.repositories.CustomerRepository;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+
 /**
  * @author pedroRhamon
  */
+@ApplicationScoped
 public class CustomerService {
+	
+	@Inject
+	CustomerRepository customerRepository;
+	
+	public List<Customer> findAllCustomers() {
+		return this.customerRepository.findAll().list();
+	}
+	
+	@Transactional
+	public void addCustomer(Customer customer) {
+		this.customerRepository.persist(customer);
+	}
 
 }
